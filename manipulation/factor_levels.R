c3b059c5fe39f8f91608e66240a9013de72e4fe6
Andriy V. Koval <koval.andrey@gmail.com>
2018-08-30 19:50:34 -0700

    kicked off greeter + factor levels

---
+++ manipulation/factor_levels.R
@@ -0,0 +1,32 @@
+
+abderr <- list(
+  "levels" = c(
+    "1"  = "Aboriginal Identity"
+    ,"2" = "Non-Aboriginal Identity"
+    )
+  ,"description"= "
+  Aboriginal identity status (detailed measure): Refers to those persons who reported identifying who reported identifying with at least one Aboriginal group (North American Indian, Métis or Inuit
+  "
+  )
+
+
+abident <- list(
+  "levels" = c(
+    "1"= "North American Indian single response"
+    ,"2" = "Metis single response"    
+    ,"3" = "Inuit single response"    
+    ,"4" = "Multiple Aboriginal identity responses"                     
+    ,"5" = "Aboriginal responses not included elsewhere"                           
+    ,"6" = "Non-Aboriginal identity population" 
+  )
+  ,"description"= "
+  Aboriginal identity status (detailed measure): Refers to those persons who reported identifying who reported identifying with at least one Aboriginal group (North American Indian, Métis or Inuit)
+  "
+  )
+
+
+
+
+
+factor_levels <- list(abderr, abident)
+
